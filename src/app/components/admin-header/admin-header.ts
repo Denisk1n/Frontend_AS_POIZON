d33d7cf63e33ec60d4efac8392063436c939b2e5
Andrew <deniskin.andrey37@gmail.com>
2026-04-09 01:51:12 +0300

    не много перенастроил визуал админки: 1) отображется кнопка добавить товара только в таблице 2) подтягиваются правильные названия 3) отображается на какой мы сейчас странице

--- src/app/components/admin-header/admin-header.ts
+++ src/app/components/admin-header/admin-header.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-header.html',
   styleUrl: './admin-header.css',
 })
-export class AdminHeader {
+
+export class AdminHeader implements OnInit, OnDestroy {
+  showAddButton: Boolean = true;
+  header_text: string = 'Товары';
+  private routerSubscription!: Subscription;
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit(){
+    this.routerSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.updateUI(); // Вызывается при КАЖДОМ переходе
+      });
+  }
+
+
+  private updateUI() {
+    const currentUrl = this.router.url;
+    console.log(currentUrl);
+
+    if (currentUrl.includes('admin/main-table')) {
+      this.showAddButton = true;
+      this.header_text = "Товары";
+    } else {
+      this.showAddButton = false;
+      this.header_text = "Редактирование товара";
+    }
+
+    console
+  }
+
+
+  ngOnDestroy() {
+    // Отписываемся при уничтожении компонента
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
+
 
 }
